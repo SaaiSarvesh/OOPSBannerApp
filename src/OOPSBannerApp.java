a9ed10b8698aebc1fd3b23f507c00f7a8c34bd5d
SaaiSarvesh <saaisarveshanandkumar@gmail.com>
2026-02-22 15:49:47 +0530

    feature (UC2): print OOPS banner using stars

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,5 +1,16 @@
+/**
+ * @author saai sarvesh
+ * @version 2.0
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // We are printing the word OOPS using 7 lines of stars!
+        System.out.println(" **** **** **** **** ");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * * * **** ");
+        System.out.println("* * * * ***** *");
+        System.out.println("* * * * * *");
+        System.out.println("* * * * * * *");
+        System.out.println(" **** **** * **** ");
     }
 }
