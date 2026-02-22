3e6476e31ba80297b8fc1cb4704d639e8a216d38
SaaiSarvesh <saaisarveshanandkumar@gmail.com>
2026-02-22 16:17:13 +0530

    feature (UC4): use array and loops for banner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,12 +1,16 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // We use String.join to combine spaces and stars efficiently!
-        System.out.println(String.join("", " ****", " ****", " ****", " **** "));
-        System.out.println(String.join("", "* *", "* *", "* *", "* "));
-        System.out.println(String.join("", "* *", "* *", "* *", "**** "));
-        System.out.println(String.join("", "* *", "* *", "*****", "    *"));
-        System.out.println(String.join("", "* *", "* *", "* ", "    *"));
-        System.out.println(String.join("", "* *", "* *", "* ", "    *"));
-        System.out.println(String.join("", " ****", " ****", "* ", " **** "));
+        String[] banner = {
+            String.join("", " ****", " ****", " ****", " **** "),
+            String.join("", "* *", "* *", "* *", "* "),
+            String.join("", "* *", "* *", "* *", "**** "),
+            String.join("", "* *", "* *", "*****", "    *"),
+            String.join("", "* *", "* *", "* ", "    *"),
+            String.join("", "* *", "* *", "* ", "    *"),
+            String.join("", " ****", " ****", "* ", " **** ")
+        };
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
