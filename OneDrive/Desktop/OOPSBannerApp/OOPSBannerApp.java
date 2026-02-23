aa12945ba6bcca0bce927f29cd0848c4f7f2ecd2
SaaiSarvesh <saaisarveshanandkumar@gmail.com>
2026-02-23 11:15:56 +0530

    Prints OOPS to console1

--- OneDrive/Desktop/OOPSBannerApp/OOPSBannerApp.java
+++ OneDrive/Desktop/OOPSBannerApp/OOPSBannerApp.java
@@ -1,5 +1,13 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
+        // We are printing the word OOPS using 7 lines of stars!
+        System.out.println(" **** **** **** **** ");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * * * **** ");
+        System.out.println("* * * * ***** *");
+        System.out.println("* * * * * *");
+        System.out.println("* * * * * * *");
+        System.out.println(" **** **** * **** ");
     }
 }
