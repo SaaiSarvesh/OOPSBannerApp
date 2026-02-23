6e96665615bc33930723281ac9e527e4c69d9eac
SaaiSarvesh <saaisarveshanandkumar@gmail.com>
2026-02-23 11:21:31 +0530

    Prints OOPS in Banner

--- OneDrive/Desktop/OOPSBannerApp/OOPSBannerApp.java
+++ OneDrive/Desktop/OOPSBannerApp/OOPSBannerApp.java
@@ -1,13 +1,11 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-        // We are printing the word OOPS using 7 lines of stars!
-        System.out.println(" **** **** **** **** ");
-        System.out.println("* * * * * * * ");
-        System.out.println("* * * * * * **** ");
-        System.out.println("* * * * ***** *");
-        System.out.println("* * * * * *");
-        System.out.println("* * * * * * *");
-        System.out.println(" **** **** * **** ");
-    }
+public class OOPSBanner {
+	public static void main(String[] args) {
+		System.out.println("   ***   "+" "+"   ***   "+" "+" ******* "+" "+"  ****** ");
+		System.out.println(" **   ** "+" "+" **   ** "+" "+" **   ** "+" "+" **    **");
+		System.out.println("**     **"+" "+"**     **"+" "+" **   ** "+" "+" **      ");
+		System.out.println("**     **"+" "+"**     **"+" "+" ******* "+" "+"  ****** ");
+		System.out.println("**     **"+" "+"**     **"+" "+" **      "+" "+"       **");
+		System.out.println(" **   ** "+" "+" **   ** "+" "+" **      "+" "+" **    **");
+		System.out.println("   ***   "+" "+"   ***   "+" "+" **      "+" "+"  ****** ");
+	}
 }
